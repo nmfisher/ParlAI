8dc31b5c1cb8d535583a56bf04faa868ceaed01f
Nick Fisher <nick.fisher@avinium.com>
2019-09-11 12:30:31 +1000

    add eval step

--- parlai/agents/response_selection_2/response_selection_2.py
+++ parlai/agents/response_selection_2/response_selection_2.py
@@ -5,7 +5,7 @@
 http://parl.ai/static/docs/tutorial_seq2seq.html
 """
 
-from parlai.core.torch_agent import TorchAgent, Output
+from parlai.core.torch_agent import TorchAgent, Output, Batch
 from parlai.core.utils import padded_tensor
 from pytorch_transformers.tokenization_distilbert import DistilBertTokenizer
 from pytorch_transformers.modeling_distilbert import DistilBertModel
@@ -15,13 +15,11 @@ from torch import optim
 import torch.nn as nn
 import torch.nn.functional as F
 
-class EncoderRNN(nn.Module):
+class AttentionFF(nn.Module):
     """Encodes the input context."""
 
-    def __init__(self, input_size, hidden_size, numlayers):
+    def __init__(self, hidden_size, numlayers):
         """Initialize encoder.
-
-        :param input_size: size of embedding
         :param hidden_size: size of GRU hidden layers
         :param numlayers: number of GRU layers
         """
@@ -29,9 +27,18 @@ class EncoderRNN(nn.Module):
         self.hidden_size = hidden_size
         self.embedder = DistilBertModel.from_pretrained('distilbert-base-uncased')
 
-        self.gru = nn.GRU(
-            input_size, hidden_size, num_layers=numlayers, batch_first=True
+        self.attn = nn.MultiheadAttention(
+            768, 2
         )
+      
+        self.key = nn.Linear(768, hidden_size)
+        self.value = nn.Linear(768, hidden_size)
+
+        self.dense = nn.Linear(
+            768, hidden_size,
+        )
+        self.activation = nn.ReLU()
+        self.linear = nn.Linear(hidden_size, 3)
 
     def forward(self, input, hidden=None):
         """Return encoded state.
@@ -39,41 +46,18 @@ class EncoderRNN(nn.Module):
         :param input: (batchsize x seqlen x embedding_dim) tensor of token indices.
         :param hidden: optional past hidden state
         """
-        input = self.embedder(torch.squeeze(input))[0]
-        output, hidden = self.gru(input, hidden)
-        return output, hidden
+        embedded1 = self.embedder(input[:,0])[0]
+        embedded2 = self.embedder(input[:,1])[0]
+        embedded3 = self.embedder(input[:,1])[0]
+        attended1 = self.attn(embedded1, self.key(embedded1), self.value(embedded1))[0].sum(1)
+        attended2 = self.attn(embedded2, self.key(embedded2), self.value(embedded2))[0].sum(1)
 
-class FF(nn.Module):
-    """Encodes the input context."""
+        attended = torch.stack([attended1, attended2], 1)
 
-    def __init__(self, hidden_size, embeddingsize, numcandidates):
-        """Initialize encoder.
-
-        :param input_size: size of embedding
-        :param hidden_size: size of GRU hidden layers
-        :param numlayers: number of GRU layers
-        """
-        super().__init__()
-        self.input_size = 2 * embeddingsize
-        self.hidden_size = hidden_size
-
-        self.dense = nn.Linear(
-            self.input_size, hidden_size,
-        )
-        self.activation = nn.ReLU()
-        self.linear = nn.Linear(hidden_size, 2)
-
-    def forward(self, input):
-        """Return encoded state.
-
-        :param input: (batchsize x seqlen) tensor of token indices.
-        :param hidden: optional past hidden state
-        """
-        # use the last sequence output as the input
         output = self.linear(
           self.activation(
             self.dense(
-              input
+              attended
             )
           )
         )
@@ -86,18 +70,13 @@ class ResponseSelection2Agent(TorchAgent):
     <http://pytorch.org/tutorials/intermediate/seq2seq_translation_tutorial.html>`_.
     """
 
-    def save(self, path=None):
-      print("Saving!")
-      super().save(path)
-
     def state_dict(self):
         """
         Get the state dict for saving.
         Override this method for more specific saving.
         """
         states = {}
-        states['encoder'] = self.encoder.state_dict()
-        states['ff'] = self.ff1.state_dict()
+        states['model'] = self.model.state_dict()
 
         if hasattr(self, 'optimizer'):
             # save optimizer params
@@ -173,6 +152,8 @@ class ResponseSelection2Agent(TorchAgent):
         """
         super().__init__(opt, shared)
 
+        self.step = 0
+
         self.id = 'ResponseSelection2Agent'
 
         if not shared:
@@ -181,21 +162,16 @@ class ResponseSelection2Agent(TorchAgent):
             nl = opt['numlayers']
             esz = opt['embeddingsize']
             nc = opt["numcandidates"]
-            # encoder converts batch_size * seqlen * embedding_dim to batch_size * embedding_dim
-            self.encoder = EncoderRNN(esz, hsz, nl)
-            self.model = self.encoder
-            # FF1 converts batch_size * embedding_dim to batch_size * num_candidates
-            self.ff1 = FF(hsz, esz, nc)
+            self.model = AttentionFF(hsz, nl)
 
             if self.use_cuda:  # set in parent class
-                self.encoder = self.encoder.cuda()
-                self.ff1 = self.ff1.cuda()
+                self.model = self.model.cuda()
 
             if opt.get('numthreads', 1) > 1:
-                self.encoder.share_memory()
-        elif 'encoder' in shared:
+                self.model.share_memory()
+        elif 'model' in shared:
             # copy initialized data from shared table
-            self.encoder = shared['encoder']
+            self.model = shared['model']
 
         # set up the criterion
         self.criterion = nn.CrossEntropyLoss()
@@ -203,7 +179,7 @@ class ResponseSelection2Agent(TorchAgent):
         # set up optims for each module
         lr = opt['learningrate']
         self.optims = {
-            'encoder': optim.SGD(self.encoder.parameters(), lr=lr),
+            'model': optim.SGD(self.model.parameters(), lr=lr),
         }
 
         self.longest_label = 1
@@ -240,7 +216,8 @@ class ResponseSelection2Agent(TorchAgent):
         Set the 'label_vec' field in the observation.
         Useful to override to change vectorization behavior
         """
-        obs["labels_vec"] = self._vectorize(obs["labels"][0])
+        if "labels" in obs:
+          obs["labels_vec"] = self._vectorize(obs["labels"][0])
         return obs
 
     def zero_grad(self):
@@ -256,15 +233,13 @@ class ResponseSelection2Agent(TorchAgent):
     def share(self):
         """Share internal states."""
         shared = super().share()
-        shared['encoder'] = self.encoder
-        shared['ff1'] = self.ff1
-      
+        shared['model'] = self.model
         return shared
 
     def report(self):
-      print(self.metrics)
       metrics = super().report()
       metrics['loss'] = self.agg_loss.item()
+      metrics['step'] = self.step
       self.agg_loss = 0
       return metrics
 
@@ -295,7 +270,6 @@ class ResponseSelection2Agent(TorchAgent):
         return super().vectorize(*args, **kwargs)
 
     def _extract_and_tokenize(self, candidates, correct):
-      candidates_toks = []
       selected = []
       for j in range(self.numcandidates - 1):
         candidate = None
@@ -311,6 +285,46 @@ class ResponseSelection2Agent(TorchAgent):
       selected.append(correct)
       return candidates_toks, selected
 
+    def _tokenize_observation(self, batch):
+        bsz = len(batch.observations)
+        inputs = []
+        max_seq_len = 0
+        candidates = []
+        for i in range(bsz):
+          utterance = batch.observations[i]["text"]
+          c_t = batch.observations[i]["labels"][0]
+          c_f = batch.observations[i]["label_candidates"][random.randint(0, len(batch.observations[i]["label_candidates"]) - 1)]
+          c1 = utterance + " [SEP] " + c_t
+          c2 = utterance + " [SEP] " + c_f
+          c1_t = self.tokenizer.encode(c1)
+          c2_t = self.tokenizer.encode(c2)
+          max_seq_len = max(max_seq_len, max(len(c1_t), len(c2_t)))
+          inputs.append([c1_t, c2_t])
+          candidates.append([c_t, c_f])
+        for c in inputs:
+          while len(c[0]) < max_seq_len:
+            c[0].append(0)
+          while len(c[1]) < max_seq_len:
+            c[1].append(0)
+        inputs = torch.LongTensor(inputs)
+        return inputs, candidates
+
+    def _get_predictions(self, output, candidates, bsz):  
+        preds = []
+        pred_text = []
+
+        for i in range(bsz):
+          pred_idx = torch.max(output[i], 0)[1] # predicted binary value for each candidate, should be [1,0]
+          if pred_idx[0].item() == 1 and pred_idx[1].item() == 0:
+            pred_text.append(candidates[i][0])
+          else:
+            pred_text.append(candidates[i][1])
+          #print("###### utterance: " + batch.observations[i]["text"])
+          #print("####### correct response: " + batch.observations[i]["labels"][0])
+          #print("######### predicted response: " + pred_text[-1])
+        #  print("########## predicted index: " + str(pred_idx))
+        return pred_text
+
     def train_step(self, batch):
         """Train model to produce ys given xs.
 
@@ -320,89 +334,42 @@ class ResponseSelection2Agent(TorchAgent):
         Return estimated responses, with teacher forcing on the input sequence
         (list of strings of length batchsize).
         """
+        self.step += 1
+
+        if self.step % 1000 == 0:
+          self.eval_step(batch)
+
         xs = batch.text_vec
 
         if xs is None:
             return
 
         bsz = xs.size(0)
-
-        # xs are the tokenized utterances
-        xs = torch.unsqueeze(xs, 1) # transform to bsz x 1 x seqlen
-        # now encode and take the last RNN output
-        xs = self.encoder(xs)[0]
-        xs = xs[:,-1] # bsz x esz
-        xs = torch.unsqueeze(xs, 1) # bsz x 1 x esz, so we can tile on dim1 later
-        
-        inputs = torch.zeros([bsz, self.numcandidates, self.embeddingsize * 2])
-
-        candidates = []
-
-        for i in range(bsz):
-          obs = batch.observations[i]
-          # tokenize/embed the responses
-          toks, selected = self._extract_and_tokenize(obs["label_candidates"], obs["labels"][0]) # returns numcandidates x seqlen
-          candidates.append(selected)
-          encoded = self.encoder(toks.cuda())[0]
-          encoded = torch.squeeze(encoded[:,-1], 1) # should be numcandidates x 1 x esz
-          
-          # tile the utterance to numcandidates
-          # xs[i] is 1 x esz
-          x = xs[i].expand(self.numcandidates, self.embeddingsize) # convert esz to (numcandidates x esz
-          encoded = torch.cat([x, encoded], 1) # numcandidates x esz
-          inputs[i] = encoded
+        inputs, candidates = self._tokenize_observation(batch)
 
         # ys will be the correct (embedded) candidate responses
-        ys = [0] * (self.numcandidates - 1)
-        ys += [1]
-        ys = [ys] * bsz
+        ys = [[1,0]] * bsz
         ys = torch.LongTensor(ys)
-        
 
         if self.use_cuda:  # set in parent class
           ys = ys.cuda()
-          inputs.cuda()
+          inputs = inputs.cuda()
 
         loss = 0
         self.zero_grad()
-        self.encoder.train()
+        self.model.train()
 
-        # Feed the concatenated inputs to the input to the FF network
-        # the output will be trained to minimize MSE against the correct candidate vector
-        ff_output = self.ff1(inputs.cuda())
-        # CrossEntropyLoss expects bsz x num_classes x ...., to transpose
-        ff_output = torch.transpose(ff_output, 1, 2)
-        #print("ys")
-        #print(ys.size())
-        #print(ys)
-        loss = self.criterion(ff_output, ys)
+        output = self.model(inputs) # returns bsz x 2 x 2
+
+        # CrossEntropyLoss expects bsz x num_classes x ...., so first transpose
+        output = torch.transpose(output, 1, 2)
+        loss = self.criterion(output, ys)
         self.agg_loss += loss
         loss.backward()
         self.update_params()
 
-        preds = []
-        #print("ff_output")
-        #print(ff_output.size()) # should be bsz x numcandidates x 2
-        #print(ff_output)
-        pred_text = []
+        pred_text = self._get_predictions(output, candidates, bsz)
 
-        for i in range(bsz):
-          #print("logits")
-          #print(ff_output[i])  # is numcandidates x 2
-          pred_idx = torch.max(ff_output[i], 0)[0] # to get max logit per candidate
-          print("pred_idx1")
-          print(pred_idx.size())
-          pred_idx = torch.max(pred_idx, 0)[1].item() # to get max candidate index per utterance
-          #print("pred_idx")
-          #print(pred_idx)
-          prediction = candidates[i][pred_idx]
-
-        #  print("###### utterance: " + batch.observations[i]["text"])
-        #  print("####### correct response: " + batch.observations[i]["labels"][0])
-        #  print("######### predicted response: " + prediction)
-        #  print("########## predicted index: " + str(pred_idx))
-          pred_text.append(prediction)
-         #        print(pred_text)
         return Output(pred_text)
 
     def eval_step(self, batch):
@@ -413,20 +380,27 @@ class ResponseSelection2Agent(TorchAgent):
 
         Return predicted responses (list of strings of length batchsize).
         """
-        xs = batch.text_vec
-        if xs is None:
-            return
-        bsz = xs.size(0)
-        starts = self.START.expand(bsz, 1)  # expand to batch size
+        item1 = { 
+          "text":"Do you like playing, or watching sports?",
+          "labels":["I like watching sports."],
+          "label_candidates":list(batch.observations[0]["label_candidates"])
+        }
+        item1["label_candidates"].append(item1["labels"][0])
+
+        item2 = { 
+          "text":"Do you think chess counts as a sport?",
+          "labels":["Yes, I think it does."],
+          "label_candidates":list(batch.observations[0]["label_candidates"])
+        }
+        item2["label_candidates"].append(item2["labels"][0])
+        batch = Batch(observations=[item1, item2])
+        inputs, candidates = self._tokenize_observation(batch)
         # just predict
-        self.encoder.eval()
-        _encoder_output, encoder_hidden = self.encoder(xs)
-        ff_output, _ = self.ff(_encoder_output)
-
-        _, output_indices = torch.max(ff_output,1)
-
-        scores = ff_output.view(-1, ff_output.size(-1))
-        pred_text = torch.gather(batch.text, output_indices)
+        self.model.eval()
+        output = self.model(inputs.cuda())
+        pred_text = self._get_predictions(output, candidates, 2)    
+        print(pred_text)
+        print("EVALUATING")
         return Output(pred_text)
 
 
